0c7f050f446c551278504706955c44a30b060dc4
KonstantinSmirnov <k.smi@mail.ru>
2021-01-07 19:47:32 +0300

    Initial Commit

---
+++ ios_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_calculator
+//
+//  Created by Konstantin Smirnov on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
