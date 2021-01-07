4834b123f92b05b147ec91043bd89b61cce2bdfa
KonstantinSmirnov <k.smi@mail.ru>
2021-01-07 20:33:49 +0300

    design interface and define buttons

--- ios_calculator/ViewController.swift
+++ ios_calculator/ViewController.swift
@@ -8,10 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayScreen: UILabel!
+    
+    @IBAction func button0(_ sender: Any) {
+    }
+    
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+    }
+    
+    @IBAction func buttonDecimal(_ sender: Any) {
+    }
+    
+    @IBAction func buttonPlus(_ sender: Any) {
+    }
+    @IBAction func buttonMinus(_ sender: Any) {
+    }
+    
+    @IBAction func buttonMultiply(_ sender: Any) {
+    }
+    
+    @IBAction func buttonDivide(_ sender: Any) {
+    }
+    
+    @IBAction func buttonPercent(_ sender: Any) {
+    }
+    
+    @IBAction func buttonPlusMinus(_ sender: Any) {
+    }
+    
+    @IBAction func buttonReset(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
